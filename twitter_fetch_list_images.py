b2f3a28497e372a764461d0e0a00148166946fe5
Fan-Yu Kuan <fykuan@gmail.com>
2014-01-02 15:32:33 +0800

    用multithread

--- twitter_fetch_list_images.py
+++ twitter_fetch_list_images.py
@@ -7,6 +7,31 @@ import urllib
 import urllib2
 import random
 import string
+from threading import Thread
+import time
+
+def save_image(tweet):
+    if isinstance(s, basestring):
+        # 抓出內容中的http://t.co/??????????" URL，該URL會是一個twitter頁面
+        re_url = re.match('^.*(http://t.co/..........)\s*.*$', tweet)
+        if (re_url != None):
+            # 產生隨機檔名
+            rand_str = ''.join(random.choice(string.ascii_lowercase+string.digits) for x in range(10))
+            target_url = re_url.group(1)
+            print "parsing: " + target_url
+            urldata = urllib.urlopen(target_url)
+            for line in urldata.readlines():
+                # 從頁面中找出圖片的URL
+                img_url = re.match(".*data-resolved-url-large=.(https.*:large)", line)
+                if (img_url != None):
+                    print ">> Found Image URL: " + img_url.group(1)
+                    # 取得副檔名
+                    img_file_ext = re.match(".*/(.*)\.(.*):large", img_url.group(1))
+                    if(img_file_ext != None):
+                        ext = img_file_ext.group(2)
+                        # 把圖片存下來
+                        print ">>>> Saving "+img_url.group(1)+" to "+rand_str+"."+ext
+                        urllib.urlretrieve(img_url.group(1), rand_str+"."+ext)
 
 # 打開設定檔twitter.json
 try:
@@ -28,24 +53,6 @@ for i in range(0,len(lists.data)):
     for j in range(0,len(list_statuses.data)-1):
         # 抓出list中tweet的內容
         s = list_statuses.data[j].text
-        if isinstance(s, basestring):
-            # 抓出內容中的http://t.co/??????????" URL，該URL會是一個twitter頁面
-            re_url = re.match('^.*(http://t.co/..........)\s*.*$', s)
-            if (re_url != None):
-                # 產生隨機檔名
-                rand_str = ''.join(random.choice(string.ascii_lowercase+string.digits) for x in range(10))
-                target_url = re_url.group(1)
-                print "parsing: " + target_url
-                urldata = urllib.urlopen(target_url)
-                for line in urldata.readlines():
-                    # 從頁面中找出圖片的URL
-                    img_url = re.match(".*data-resolved-url-large=.(https.*:large)", line)
-                    if (img_url != None):
-                        print ">> Found Image URL: " + img_url.group(1)
-                        # 取得副檔名
-                        img_file_ext = re.match(".*/(.*)\.(.*):large", img_url.group(1))
-                        if(img_file_ext != None):
-                            ext = img_file_ext.group(2)
-                            # 把圖片存下來
-                            print ">>>> Saving "+img_url.group(1)+" to "+rand_str+"."+ext
-                            urllib.urlretrieve(img_url.group(1), rand_str+"."+ext)
+        t = Thread(target=save_image, args=(s,))
+        time.sleep(1)
+        t.start()
